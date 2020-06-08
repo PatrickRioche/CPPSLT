061531165663ed3ac6bc29ec1f9d759987ce573d
Rioche Patrick <patrick.rioche@gmail.com>
2020-06-08 19:41:25 +0200

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -1,1 +1,18 @@
+// HC-SR04-SR05		
+// VMA306		
+//
+// Ce module permet d'évaluer les distances entre un objet
+// mobile et les obstacles rencontrés. Il suffit d'envoyer
+// une impulsion de 10 µs en entrée et le capteur renvoie
+// une largeur d'impulsion proportionnelle à la distance.
+//
+// Alimentation: 5 Vcc
+// Consommation: 15 mA
+// Fréquence: 40 kHz
+// Portée: de 2 cm à 4 m
+// Déclenchement: impulsion TTL positive de 10µs
+// Signal écho: impulsion positive TTL proportionnelle à la distance
+// Calcul: distance (cm) = impulsion (µs) / 58
+// Dimensions: 45 x 21 x 18 mm
+  
 print("Hello Sensor");
